825be3bfeae57b37195e2d06fd5036b8578e7939
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-16 16:13:48 +0200

    Agrege informacion

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Micomponente from './componentes/Micomponente';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola roci</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const container =document.getElementById('root')
+
+ReactDOM.render(<Micomponente/>,container)
+
---
+++ src/componentes/Micomponente.js
@@ -0,0 +1,13 @@
+//componentes de clase
+import React,{Component} from 'react';
+
+class Micomponente extends Component{
+
+    render(){
+        return(
+            <h1> Hola LLamo al MiComponente nuevo</h1>
+    
+        );
+    }
+}
+export default Micomponente;
