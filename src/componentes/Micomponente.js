1b41bf29157f1f916ddeb08812d1900168c9ce6b
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-16 16:58:44 +0200

    Agrege informacion

--- src/componentes/Micomponente.js
+++ src/componentes/Micomponente.js
@@ -1,13 +1,13 @@
 //componentes de clase
 import React,{Component} from 'react';
 
-class Micomponente extends Component{
+class Micomponente extends React.Component{
 
     render(){
-        return(
+        return
             <h1> Hola LLamo al MiComponente nuevo</h1>
     
-        );
+        
     }
 }
 export default Micomponente;
