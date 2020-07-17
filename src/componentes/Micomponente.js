da95818c84ec0ed77742f365403732b849fbdf94
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-17 10:40:33 +0200

    Agrege informacion

--- src/componentes/Micomponente.js
+++ src/componentes/Micomponente.js
@@ -1,6 +1,7 @@
 //componentes de clase
 import React from 'react';
 import exerciseImg from '../img/exercise.png'
+import './styles/Micomponente.css'
 
 
 
@@ -8,18 +9,20 @@ class Micomponente extends React.Component {
 
     render() {
         return (
-            <div> 
-                <div >
-                    <img src={exerciseImg} />
-                </div>
-                <div>
-                    <h1> Hola </h1>
-                    <p> Learn</p>
-                    <p> Learn</p>
+            <div className="card mx-auto Fitness-card">
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6" >
+                            <img src={exerciseImg} />
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1> Hola </h1>
+                            <p> Learn</p>
+                            <p> Learn</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-
 
         );
     }
