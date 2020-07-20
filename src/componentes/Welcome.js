dda988cbdc8dda0c796b979138e48dfe2cd418f6
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-20 12:41:44 +0200

    Agrege informacion

---
+++ src/componentes/Welcome.js
@@ -0,0 +1,18 @@
+//componentes funcionales
+
+import React from 'react'
+import './styles/Welcome.css'
+
+function Welcome(props){
+
+    return(
+        <div className="container">
+            <div className="Fitness-User-Info">
+                <h1>Hello {props.username}</h1>
+                <p>Bienvenido</p>
+                </div> 
+        </div>
+    )
+}
+
+export default Welcome
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Micomponente from './componentes/Micomponente'
+import Welcome from './componentes/Welcome'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 
 const container =document.getElementById('root')
 
-ReactDOM.render(<Micomponente/>,container)
+ReactDOM.render(<div>
+                <Welcome
+                username="Rocibel"/>
+                <Micomponente
+                    title="Hola"
+                    description=""
+                    img=""
+                    leftColor=""
+                    rightColor=""
+                  />
+                  </div>,container)
