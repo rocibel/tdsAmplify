1b3b397dd4158d175b4b7a0c6d18681cd4823623
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-20 13:11:34 +0200

    Agrege informacion

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Micomponente from './componentes/Micomponente'
-import Welcome from './componentes/Welcome'
+import Ejercicio from './pages/Ejercicio'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 
 const container =document.getElementById('root')
 
-ReactDOM.render(<div>
-                <Welcome
-                username="Rocibel"/>
-                <Micomponente
-                    title="Hola"
-                    description=""
-                    img=""
-                    leftColor=""
-                    rightColor=""
-                  />
-                  </div>,container)
+ReactDOM.render(<Ejercicio></Ejercicio>,container)
---
+++ src/pages/Ejercicio.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ReactComponent } from '*.svg';
+import Micomponente from '../componentes/Micomponente'
+import Welcome from '../componentes/Welcome'
+
+
+class Ejercicio extends ReactComponent{
+
+    render(){
+return(
+    <div>
+                <Welcome
+                username="Rocibel"/>
+                <Micomponente
+                    title="Hola"
+                    description=""
+                    img=""
+                    leftColor=""
+                    rightColor=""
+                  />
+                  </div>
+)
+    }
+}
+
+export default Ejercicio
